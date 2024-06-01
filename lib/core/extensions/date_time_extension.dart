308e94d14dcd7d51928b307c5adabc3e369f73ac
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-06-01 11:25:19 +0300

    add to locale to date time

--- lib/core/extensions/date_time_extension.dart
+++ lib/core/extensions/date_time_extension.dart
@@ -19,13 +19,25 @@ extension FormatDateTime on DateTime {
     'Dec'
   ];
 
+  int get currentYear => toLocal().year;
+
+  int get currentMonth => toLocal().month;
+
+  int get currentDay => toLocal().day;
+
+  int get currentHour => toLocal().hour;
+
+  int get currentMinute => toLocal().minute;
+
+  int get currentSecond => toLocal().second;
+
   /// example: 27-dec,596
   String get dMMMyyyy {
-    return "$day-${months[month - 1]},$year";
+    return "$currentDay-$currentMonth,$currentYear";
   }
 
   /// example: 27-dec,2024, 3:40:22
   String get dMMMyyyyHMS {
-    return "$day-${months[month - 1]},$year, $hour:$minute:$second";
+    return "$currentDay-$currentMonth,$currentYear, $currentHour:$currentMinute:$currentSecond";
   }
 }
